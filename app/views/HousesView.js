cadf2f7a58e70bdc2c146309974cc32fd27b36c4
Riley <riley@idahosuperiorcustoms.com>
2023-06-21 16:53:35 -0600

    Updates

---
+++ app/views/HousesView.js
@@ -0,0 +1,16 @@
+export const HousesView = /*html*/`
+<div class="container-fluid">
+<section class="row">
+<div class="d-flex justify-content-between pt-3">
+<h1>Available Homes</h1>
+<button class="btn btn-primary ms-3" type="button" data-bs-toggle="collapse" data-bs-target="#homeCollapse" aria-expanded="false" aria-controls="collapseExample">
+New Home Listing Form
+</button>
+</div>
+</section>
+<section class="row d-flex" id="houseListings"></section>
+</div>
+
+
+
+`
